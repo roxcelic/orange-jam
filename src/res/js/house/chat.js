17747d0ea475c9cf6333159aa075ea7d49879312
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-15 19:57:56 +0100

    fully fixed chat

--- src/res/js/house/chat.js
+++ src/res/js/house/chat.js
@@ -2,26 +2,51 @@ function spawnMessage(message){
     let newMessage = document.createElement("P");
     newMessage.textContent = message[0];
     newMessage.style.color = message[1];
-
+    newMessage.id = message[2];
     window.scrollTo(0, document.body.scrollHeight);
     return newMessage;
 }
 
 function getOldChat(chat){
-    let fullchatlog = chat.childNodes.length;
+    chat = [...chat.children];
+
+    let fullchatlog = chat.map((message) => [message.textContent, message.style.color, message.id]);
+
     return fullchatlog;
 }
 
-async function displayChat(interval = null) {
-    let chat = document.getElementById("chat");
+async function displayChat(interval = null, chat = null, speed = null, delay = 0) {
+    if (speed != null && interval != null && speed.value != delay){
+        console.log("changing chat refresh speed");
+        clearInterval(interval);
+        loadChat(speed.value);
+        console.log("changed chat value");
+        return null;
+    }
 
     try {
         let response = await fetch("https://api.roxcelic.love/api/v1/chat/view")
         let data = await response.json();
-        data.chat = Array.isArray(data.chat) ? data.chat : [["basic message", "#783432"]];
+        data.chat = Array.isArray(data.chat) ? data.chat : [["basic message", "#783432", 0]];
 
-        let difference = data.chat.slice(getOldChat(chat));
-        
+        let previousMessages = getOldChat(chat);
+        let difference = [];
+        let index = 0;
+
+        if (previousMessages[0] == undefined){
+            difference = data.chat;
+        } else if (previousMessages[previousMessages.length - 1][2] != data.chat[data.chat.length - 1][2]){  
+            let finalSnow = previousMessages[previousMessages.length - 1][2];
+              
+            data.chat.forEach((message, ChatIndex) => {
+                if (message[2] == finalSnow) index = ChatIndex + 1;
+            });
+
+            difference = data.chat.slice(index);
+        } else {
+            console.log("unchaged");
+        }
+
         difference.forEach(message => {
             chat.appendChild(spawnMessage(message));
         });
@@ -33,10 +58,15 @@ async function displayChat(interval = null) {
     }
 }
 
-export async function loadChat() {
+export async function loadChat(delay = 0) {
+    let chat = document.getElementById("chat");
+    let speed = document.getElementById("chatSpeed");
+    delay = speed.value;
+
     let display = setInterval(() => {
-        displayChat(display, [])
-    }, 1000); 
+        displayChat(display, chat, speed, delay);
+        console.log("refreshed chat");
+    }, delay); 
 }
 
 export async function sendMessage() {
@@ -56,7 +86,8 @@ export async function sendMessage() {
             body: JSON.stringify(data)
         });
 
-        displayChat();
+        let chat = document.getElementById("chat");
+        displayChat(null, chat);
     } catch (e) {
         console.log(e);
     }
--- src/res/js/script.js
+++ src/res/js/script.js
@@ -11,6 +11,7 @@ async function setup(params) {
     console.log(`loding '${window.location.pathname}' content`);
     switch (window.location.pathname){
         case "/chat/" || "/chat" || "chat":
+
             await loadChat();
             document.getElementById("chatSend").addEventListener("click", sendMessage);
 
@@ -24,8 +25,15 @@ async function setup(params) {
             let color = document.getElementById("chatColor");
             color.value = document.cookie.split('; ').find(row => row.startsWith('color='))?.split('=')[1];
 
+            let speed = document.getElementById("chatSpeed");
+            speed.value = parseInt(document.cookie.split('; ').find(row => row.startsWith('speed='))?.split('=')[1]);
+
             color.addEventListener('input', function() {
                 document.cookie = `color=${this.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+            });
+
+            speed.addEventListener('input', function() {
+                document.cookie = `speed=${this.value}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
             });
 
             break;
