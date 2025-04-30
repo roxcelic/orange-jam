313b2c995f89d03f623976e5dd24f79f98b86bc4
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-30 22:17:32 +0100

    another fix ' §:q '

--- src/res/js/house/chat.js
+++ src/res/js/house/chat.js
@@ -1,27 +1,31 @@
 function spawnMessage(message){
-    const formattedDate = new Intl.DateTimeFormat('en-GB', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric'
-    }).format(new Date(message[4]));
-
-    let pappaMessage = document.createElement("P");
-    pappaMessage.style.fontSize = "xx-small";
-    pappaMessage.style.marginTop = "5px";
-    pappaMessage.style.marginLeft = "5px";
-    pappaMessage.textContent = `${message[3]}--${formattedDate}--${new Date(message[4]).toTimeString().split(' ')[0]}`;
-    pappaMessage.id = message[2];
-
-    let newMessage = document.createElement("P");
-    newMessage.style.fontSize = "medium";
-    newMessage.style.marginLeft = "-5px";
-    newMessage.textContent = message[0];
-    newMessage.style.color = message[1];
-
-    window.scrollTo(0, document.body.scrollHeight);
-
-    pappaMessage.appendChild(newMessage);
-    return pappaMessage;
+    try {
+        const formattedDate = new Intl.DateTimeFormat('en-GB', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        }).format(new Date(message[4]));
+    
+        let pappaMessage = document.createElement("P");
+        pappaMessage.style.fontSize = "xx-small";
+        pappaMessage.style.marginTop = "5px";
+        pappaMessage.style.marginLeft = "5px";
+        pappaMessage.textContent = `${message[3]}--${formattedDate}--${new Date(message[4]).toTimeString().split(' ')[0]}`;
+        pappaMessage.id = message[2];
+    
+        let newMessage = document.createElement("P");
+        newMessage.style.fontSize = "medium";
+        newMessage.style.marginLeft = "-5px";
+        newMessage.textContent = message[0];
+        newMessage.style.color = message[1];
+    
+        window.scrollTo(0, document.body.scrollHeight);
+    
+        pappaMessage.appendChild(newMessage);
+        return pappaMessage;
+    } catch (e) {
+        return document.createElement("p");
+    }
 }
 
 function getOldChat(chat){
@@ -33,7 +37,7 @@ function getOldChat(chat){
 }
 
 async function displayChat(interval = null, chat = null, speed = null, delay = 0) {
-    let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
+    let apiRoot = `https://api.roxcelic.love/`;
 
     if (speed != null && interval != null && speed.value != delay){
         console.log("changing chat refresh speed");
@@ -89,7 +93,7 @@ export async function loadChat(delay = 0) {
 }
 
 export async function sendMessage() {
-    let apiRoot = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':3000' : ''}/`;
+    let apiRoot = `https://api.roxcelic.love/`;
 
     let data = {
         upload: document.getElementById("chatMessage").value,
