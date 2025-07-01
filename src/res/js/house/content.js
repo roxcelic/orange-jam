aaf4bf2554548949f0a06c59251c858c23543948
Roxie.Rowbotham-G (12M) <Roxie.Rowbotham-G@utcmediacityuk.org.uk>
2025-07-01 14:59:06 +0100

    hehehaw

---
+++ src/res/js/house/content.js
@@ -0,0 +1,30 @@
+export let documentContent = {
+    "Home": {
+        items: [
+            {
+                elementId: "title",
+                speed: 5,
+                stop: false,
+                signifier: "_"
+            },
+            {
+                elementId: "content",
+                speed: 5,
+                stop: true,
+                signifier: "_"
+            },
+            {
+                elementId: "content2",
+                speed: 5,
+                stop: true,
+                signifier: "_"
+            },
+            {
+                elementId: "content3",
+                speed: 5,
+                stop: true,
+                signifier: "_"
+            }
+        ]
+    }
+};
--- src/res/js/script.js
+++ src/res/js/script.js
@@ -1,16 +1,48 @@
-import { test, displayUpdate } from "./house/DisplayManager";
+import { documentContent } from "./house/content";
 
-let distrobuteInformation = ["1", "2", "3", "4"];
+let type = (currentText, fullText, signifier = "_") => {
+    let hadSignifier = false;
 
-async function buildWorld(params) {
-    distrobuteInformation.forEach(display => {
+    // remove the signifier
+    if (currentText[currentText.length - 1] == signifier) {
+        currentText = currentText.substring(0, currentText.length - 1);
+        hadSignifier = true;
+    }
 
-        displayUpdate(document.getElementById(`${display}menu`), document.getElementById(`${display}item`), display);
+    if (currentText != fullText) currentText += fullText[currentText.length];
 
-        document.getElementById(`${display}menu`).onchange = function() {
-            displayUpdate(document.getElementById(`${display}menu`), document.getElementById(`${display}item`), display);
-        };
-    });
+    // if next character is a space just add it
+    if (fullText[currentText.length] == " ") currentText += " ";
+
+    // re add the signifier
+    if (!hadSignifier) currentText = `${currentText}${signifier}`
+
+    return currentText;
 }
 
-buildWorld();
+let typeOutTag = (id, speed = 2.5, stop = false, signifier = "_") => {
+    // initiate
+    let element = document.getElementById(id);
+    let fullText = element.textContent;
+
+    element.textContent = element.textContent[0];
+
+    // start
+    let animationWait = getComputedStyle(element).getPropertyValue("animation-delay");
+    let wait = parseInt(animationWait.substring(0, animationWait.length - 1)) * 1000;
+
+    animationWait = getComputedStyle(element).getPropertyValue("animation-duration");
+    wait += parseInt(animationWait.substring(0, animationWait.length - 1)) * 250;
+
+    setTimeout(() => {
+            let interval = setInterval(() => {
+            element.textContent = type(element.textContent, fullText, signifier);
+
+            if (stop && element.textContent == fullText) clearInterval(interval);
+        }, (speed * 1000) / fullText.length - 1);
+    }, wait);
+}
+
+documentContent[document.title].items.forEach(element => {
+	typeOutTag(element.elementId, element.speed, element.stop, element.signifier);
+});
