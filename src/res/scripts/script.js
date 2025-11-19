557de0da9415a740fe806ada3f07b43bafded2e6
a pile of mushy (rocks?) <157179472+roxcelic@users.noreply.github.com>
2025-11-19 12:39:24 +0000

    initial

--- src/res/scripts/script.js
+++ src/res/scripts/script.js
@@ -1,1 +1,17 @@
-console.log("init");
+let fuck = async () => {
+    document.querySelectorAll('special-text').forEach((item) => {
+
+        let items = [];
+        for (let i = 0; i < item.innerText.length; i++) {
+            let newText = document.createElement("P");
+            newText.innerText = item.innerText[i] != " " ? item.innerText[i] : ".";
+            newText.style.animationDelay = `${i * 0.1}s`;
+            items.push(newText);
+        }
+
+        item.innerText = "";
+        items.forEach((newText) => item.appendChild(newText));
+    });
+}
+
+fuck();
